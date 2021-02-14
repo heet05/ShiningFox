730258f1945d0352109c9538f76cab7ca7701b76
heet <54179610+heet05@users.noreply.github.com>
2021-02-14 22:56:06 +0530

    Initial commit

---
+++ app/src/main/java/com/heet/shiningfox/UserModel.java
@@ -0,0 +1,21 @@
+package com.heet.shiningfox;
+
+public class UserModel {
+    String userMobile,uId;
+
+    public String getUserMobile() {
+        return userMobile;
+    }
+
+    public void setUserMobile(String userMobile) {
+        this.userMobile = userMobile;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+}
